40687ff906584882125931aea76d9a04e2191c94
Scott Bowler <scott.bowler@learningfuze.com>
2019-08-15 07:05:23 +0000

    Initial express server setup

--- server.js
+++ server.js
@@ -1,1 +1,13 @@
-// Node Server Code Goes Here
+const express = require('express');
+const NODE_ENV = process.env.NODE_ENV || 'development';
+const PORT = process.env.PORT || 9000;
+
+const server = express();
+
+server.get('/', (req, res) => {
+    res.send('<h1>Server is working!</h1>');
+});
+
+server.listen(PORT, () => {
+    console.log('Server listening @ PORT:' + PORT);
+});
